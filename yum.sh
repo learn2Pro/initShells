f43e64f296c2bd156228b51e69a9ee36efa38759
tang <tyqw9110@qq.com>
2016-11-18 12:29:39 +0800

    test

--- yum.sh
+++ yum.sh
@@ -6,7 +6,6 @@ echo $v
 echo ${v:15:1}
 cmd="wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-${v:15:1}.repo"
 eval $cmd
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
 yum clean all
 yum makecache
 
