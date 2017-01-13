d5d2e4a5939b5fea56e97ec03785a2a22721e8fa
root <root@localhost.localdomain>
2017-01-13 01:45:04 -0500

    加入kafka和zookeeper

--- yum.sh
+++ yum.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "============init yum list======="
+systemctl stop firewalld.service &&
+systemctl disable firewalld.service &&
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-v=`cat /etc/redhat-release`  
-echo $v  
-echo ${v:15:1}
 cmd="wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo"
 eval $cmd
 yum clean all
@@ -13,14 +12,6 @@ cd /usr/local
 pstr="$PATH"
 #####git install#########
 yum -y install readline-devel pcre-devel curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker &&
-wget https://github.com/git/git/archive/v2.3.0.zip &&
-unzip v2.3.0.zip
-cd git-2.3.0
-make prefix=/usr/local/git all
-make prefix=/usr/local/git install
-echo "# git">>/etc/bashrc
-echo "export PATH=/usr/local/git/bin:$pstr">>/etc/bashrc
-source /etc/bashrc
 ########openresty install#######
 wget https://openresty.org/download/openresty-1.11.2.1.tar.gz &&
 tar -zxvf openresty-1.11.2.1.tar.gz &&
@@ -28,14 +19,37 @@ cd openresty-1.11.2.1 &&
 echo ./configure &&
 make &&
 make install &&
-echo "# openresty">>/etc/bashrc
-echo "export PATH=/usr/local/openresty/bin:$pstr">>/etc/bashrc
+echo "### openresty ###">>/etc/bashrc &&
+echo "export PATH=/usr/local/openresty/bin:$pstr">>/etc/bashrc &&
+echo "export PATH=/usr/local/openresty/nginx/sbin:$PATH">>/etc/bashrc &&
 ########java -1.8 #################
-yum -y install java-1.8.0-openjdk-devel.i686 &&
+yum -y install java-1.8.0-openjdk-devel.x86_64 &&
+echo "# java">>/etc/bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/jre">>/etc/bashrc
+echo "export CLASSPATH=.:/$JAVA_HOME/lib:$CLASSPATH">>/etc/bashrc
+#########settings ################
+echo "# max thread">>/etc/bashrc
+echo "ulimit -u 2048">>/etc/bashrc
+echo "# ld path">>/etc/bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/opt">>/etc/bashrc
+echo "pkg path">>/etc/bashrc
+echo "export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH">>/etc/bashrc
 ########hadoop#####################
-wget http://apache.fayea.com/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz &&
+wget http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz &&
 tar -zxvf hadoop-2.6.5.tar.gz &&
 echo "export PATH=/usr/local/hadoop-2.6.5/bin:$pstr">>/etc/bashrc &&
+######## kafka ###################
+wget http://mirrors.tuna.tsinghua.edu.cn/apache/kafka/0.10.1.1/kafka_2.10-0.10.1.1.tgz &&
+tar zvf kafka_2.10-0.10.1.1.tgz &&
+mv kafka_2.10-0.10.1.1/ kafka-2.11/ &&
+echo "# kafka">>/etc/bashrc
+echo "export KAFKA_HOME=/usr/local/kafka-2.11">>/etc/bashrc
+echo "export PATH=/usr/local/kafka-2.11/bin:$PATH">>/etc/bashrc
+######## zookeeper #####################
+wget https://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz &&
+tar -zxvf zookeeper-3.4.6.tar.gz &&
+echo "# zookeeper">>/etc/bashrc
+echo "export PATH=/usr/local/zookeeper-3.4.6/bin:$PATH">>/etc/bashrc
 ####### python pip ###############
 yum -y install python-devel python &&
 wget https://bootstrap.pypa.io/get-pip.py &&
